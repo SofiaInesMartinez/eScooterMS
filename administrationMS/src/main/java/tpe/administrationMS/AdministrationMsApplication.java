7411d13ed8f5859cd017b20217bf9c4a2c941fa0
Sofia Martinez <sofiainesmartinez@gmail.com>
2023-10-24 18:47:23 -0300

    start AdministrationMS

---
+++ administrationMS/src/main/java/tpe/administrationMS/AdministrationMsApplication.java
@@ -0,0 +1,13 @@
+package tpe.administrationMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdministrationMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdministrationMsApplication.class, args);
+	}
+
+}
