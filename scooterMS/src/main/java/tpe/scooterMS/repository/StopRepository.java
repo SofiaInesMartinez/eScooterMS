d11bf8340456c7bbfb078eafae46e0242ff69d93
Garci270 <agustingarciaamaro@gmail.com>
2023-10-26 08:52:43 -0300

    fix delete stop by id

--- scooterMS/src/main/java/tpe/scooterMS/repository/StopRepository.java
+++ scooterMS/src/main/java/tpe/scooterMS/repository/StopRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -18,6 +19,7 @@ public interface StopRepository extends JpaRepository<Stop, Integer>{
 	@Query("SELECT s FROM Stop s ORDER BY s.id DESC")
 	public List<Stop> getStopsBySimpleOrdering();
 	
+	@Modifying
 	@Query("DELETE FROM Stop s WHERE s.id = :id")
     void deleteStopById(@Param("id") long id);
 }
